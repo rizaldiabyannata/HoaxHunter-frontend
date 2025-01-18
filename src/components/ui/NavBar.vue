9bd303a34e18fe4d6e7de32288c619e5348ac081
rizaldiabyannata <aldizar255@gmail.com>
2025-01-18 21:13:10 +0800

    style(navbar) : styling

--- src/components/ui/NavBar.vue
+++ src/components/ui/NavBar.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="flex flex-row justify-between items-center bg-zinc-500 p-4">
-    <div class="flex items-center space-x-4">
+  <div
+    class="flex flex-row justify-between font-geist items-center bg-zinc-900 py-2 px-8 border-b border-zinc-500"
+  >
+    <div class="flex flex-row space-x-6">
+      <h2 class="dark:text-white">HoaxHunter</h2>
       <router-link to="/" class="text-white">Home</router-link>
-      <router-link to="/login" class="text-white">Login</router-link>
+      <router-link to="/" class="text-white">Home</router-link>
+      <router-link to="/" class="text-white">Home</router-link>
+      <router-link to="/" class="text-white">Home</router-link>
     </div>
     <div class="flex items-center space-x-4">
+      <router-link to="/login" class="text-white">Login</router-link>
       <router-link to="/register" class="text-white">Register</router-link>
       <router-link to="/profile" class="text-white">Profile</router-link>
-    </div>
-    <div class="flex items-center">
-      <ToggleSwitchMode />
+      <div class="flex items-center">
+        <ToggleSwitchMode />
+      </div>
     </div>
   </div>
 </template>
